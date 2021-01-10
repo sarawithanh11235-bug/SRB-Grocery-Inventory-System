5e2dc8c862057ddd878739bd02876d254468d3f3
Sarah R. Blette <51761783+sarawithanh11235-bug@users.noreply.github.com>
2021-01-10 16:39:39 -0500

    Added Django Files (in progress)
    Added Django Files (in progress) - learning Django & Python

---
+++ hellodjango/myproject/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GroceryinventorysystemConfig(AppConfig):
+    name = 'myproject'
---
+++ hellodjango/myproject/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here. DJANGO SERVER
+# Email: sblet0001@mail.ct.edu
+# Username: sarah
+# PW: DryadSoraka6
+# http://127.0.0.1:8000/
+
+def hello(request):
+    return HttpResponse('<h1>Grocery Inventory System with Django!</h1>')
+
+
